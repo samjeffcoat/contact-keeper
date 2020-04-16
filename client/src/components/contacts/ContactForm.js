a9658f10a6e22ce534d20e4a58516ae1080be1f7
Sam Jeffcoat <samjeffcoat@gmail.com>
2020-04-16 16:46:26 -0500

    contact form component built and loaded into home component with dummy data

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const ContactForm = (props) => {
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    type: 'personal',
+  });
+
+  const { name, email, phone, type } = contact;
+
+  const onChange = (e) =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  return (
+    <form>
+      <h2 className='text-primary'>Add Contact</h2>
+      <input
+        type='text'
+        placeholder='Name'
+        name='name'
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        type='email'
+        placeholder='Email'
+        name='name'
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type='text'
+        placeholder='phone'
+        name='phone'
+        value={phone}
+        onChange={onChange}
+      />
+      <h5>Contact Type</h5>
+      <input
+        type='radio'
+        name='type'
+        value='personal'
+        checked={type === 'personal'}
+      />
+      Personal{' '}
+      <input
+        type='radio'
+        name='type'
+        value='Professional'
+        checked={type === 'professional'}
+      />
+      Professional
+      <div>
+        <input
+          type='submit'
+          value='Add Contact'
+          className='btn btn-primary btn-block'
+        />
+      </div>
+    </form>
+  );
+};
+
+ContactForm.propTypes = {};
+
+export default ContactForm;
