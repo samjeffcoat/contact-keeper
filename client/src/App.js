942fa746a18d46408377564d5f626a16b918b4f3
Sam Jeffcoat <samjeffcoat@gmail.com>
2020-04-16 14:17:14 -0500

    Creates default state, wraps app in ContextSTate

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,22 @@ import './App.css';
 import NavBar from './components/layout/NavBar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import ContactState from './context/contact/ContactState';
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <NavBar />
-        <div className='containter'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <div className='containter'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
